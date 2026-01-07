a2fb9ebe764924179cef9bfa6cd02d70785d824b
lisasteiger <75636934+lisasteiger@users.noreply.github.com>
2026-01-07 22:08:49 +0100

    s

--- diploma.js
+++ diploma.js
@@ -35,3 +35,42 @@ window.addEventListener(
   },
   { passive: false }
 );
+
+let startY = 0;
+let endY = 0;
+const swipeThreshold = 50; // Mindestdistanz
+
+window.addEventListener("touchstart", (e) => {
+  startY = e.touches[0].clientY;
+});
+
+window.addEventListener("touchend", (e) => {
+  endY = e.changedTouches[0].clientY;
+  handleSwipe();
+});
+
+function handleSwipe() {
+  const deltaY = startY - endY;
+
+  // Swipe nach oben
+  if (deltaY > swipeThreshold && index < texts.length - 1) {
+    changeText(index + 1);
+  }
+
+  // Swipe nach unten
+  if (deltaY < -swipeThreshold && index > 0) {
+    changeText(index - 1);
+  }
+}
+
+const story = document.getElementById("story");
+
+story.addEventListener(
+  "touchmove",
+  (e) => {
+    if (index < texts.length - 1) {
+      e.preventDefault();
+    }
+  },
+  { passive: false }
+);
