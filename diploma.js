217399d9c7801fa5683c4936cf14713aa5e1936e
lisasteiger <75636934+lisasteiger@users.noreply.github.com>
2026-01-13 12:25:39 +0100

    s

--- diploma.js
+++ diploma.js
@@ -23,7 +23,7 @@ button.addEventListener("click", async () => {
     console.error(error);
     status.textContent = error.message;
   } else {
-    status.textContent = "Text gespeichert ✔";
+    status.textContent = "danke";
     textarea.value = "";
   }
 });
